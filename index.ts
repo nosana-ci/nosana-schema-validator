f953ed29233359a9cfbebc13cf4fc6460f3b2980
djmbritt <david@effect.ai>
2022-12-08 23:48:07 +0100

    Update with yaml and make verbose

--- index.ts
+++ index.ts
@@ -1,16 +1,19 @@
 import AJV from "ajv"
-import addFormats from "ajv-formats"
-import { NosPipelineSchema } from "./schema";
+// import addFormats from "ajv-formats"
+import { NosPipelineSchema } from "./src/schema";
 import { parse } from 'yaml'
 
 // Create AJV
-const ajv = new AJV()
-addFormats(ajv)
+const ajv = new AJV({
+  allErrors: true,
+  verbose: true
+})
+// addFormats(ajv)
 
 // Create validate function
 const validateJson = ajv.compile(NosPipelineSchema)
 
-// yaml utilities
+// yaml utility
 const validateYaml = (yaml: string) => validateJson(parse(yaml))
 
 export { validateJson, validateYaml }
