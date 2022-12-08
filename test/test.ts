f59398a8891166d2b29d5b083eb6661a6f2c8717
djmbritt <david@effect.ai>
2022-12-08 23:49:22 +0100

    Add tests

---
+++ test/test.ts
@@ -0,0 +1,36 @@
+import { validateJson, validateYaml } from '../index'
+import { exampe_yaml, example_json } from './example'
+import { parse } from 'yaml'
+
+
+const validYaml = () => {
+  console.log("Validating Yaml Schema", exampe_yaml)
+  // const json = parse(exampe_yaml)
+  // const validated = validateJson(json)
+  const validated = validateYaml(exampe_yaml)
+
+  if (validated) {
+    console.log("✅ Yaml Schema Correct.")
+  } else {
+    console.log("❌ Yaml Schema Incorrect.")
+    if (validateJson.errors) {
+      validateJson?.errors?.forEach(err => console.log(err))
+    }
+  }
+}
+
+const validJson = () => {
+  console.log('Validating JSON Schema', example_json)
+
+  const validated = validateJson(example_json)
+
+  if (validated) {
+    console.log('✅ JSON Schema Correct!')
+  } else {
+    console.log('❌ JSON Schema Wrong!')
+    validateJson?.errors?.forEach(err => console.log(err))
+  }
+}
+
+validJson()
+validYaml()
