595abe5a3f7fc048212a7337b4b6310770e57b85
djmbritt <david@effect.ai>
2023-01-25 23:49:01 +0100

    Add one more test

--- test/index.test.ts
+++ test/index.test.ts
@@ -6,6 +6,15 @@ test("validate Error Json", () => {
     expect(validateJson(error_json).valid).toBe(false)
 })
 
+test("Should return an error ", () => {
+    expect(validateJson(error_json).errors).toHaveLength(1)
+})
+
+test("Should return an error on commands", () => {
+    expect(validateJson(error_json).errors?.pop()?.schemaPath)
+    .toEqual("#/properties/jobs/items/properties/commands/type")
+})
+
 test("validate Correct Json", () => {
     expect(validateYaml(correct_json).valid).toBe(true)
 })
