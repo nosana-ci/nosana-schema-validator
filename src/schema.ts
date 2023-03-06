99fdd56e330a9102e1e19e4bba3921617ae92418
Laurens Verspeek <l.verspeek@gmail.com>
2023-03-06 20:00:15 +0100

    add secrets

--- src/schema.ts
+++ src/schema.ts
@@ -1,48 +1,115 @@
-import { NosJobData } from "./types"
-import { JSONSchemaType } from 'ajv'
+import { NosJobData } from "./types";
+import { JSONSchemaType } from "ajv";
 
 export const NosPipelineSchema: JSONSchemaType<NosJobData> = {
-    type: "object",
+  type: "object",
 
-    properties: {
+  properties: {
+    nosana: {
+      type: "object",
+      nullable: false,
+      properties: {
+        description: {
+          type: "string",
+          nullable: false,
+          minLength: 3,
+          maxLength: 255,
+        },
+      },
+    },
 
-      nosana: {
+    global: {
+      type: "object",
+      nullable: false,
+      properties: {
+        // 📦️ Container image to use for all jobs
+        image: {
+          type: "string",
+          nullable: true,
+          minLength: 1,
+          maxLength: 4096,
+        },
+
+        trigger: {
+          type: "object",
+          nullable: true,
+          properties: {
+            branch: {
+              type: ["array", "string"],
+              items: { type: "string" },
+              nullable: true,
+              uniqueItems: true,
+            },
+          },
+        },
+
+        secrets: {
+          type: "array",
+          items: { type: "string" },
+          nullable: true,
+          uniqueItems: true,
+        },
+
+        environment: {
+          anyOf: [
+            {
+              type: "object",
+              nullable: true,
+              additionalProperties: { type: "string" }, // Additional properties need to be of type string.
+            },
+            {
+              type: "object",
+              nullable: true,
+              properties: {
+                type: {
+                  type: "string",
+                  nullable: false,
+                },
+                endpoint: {
+                  type: "string",
+                  nullable: false,
+                },
+                value: {
+                  type: "string",
+                  nullable: false,
+                },
+              },
+            },
+          ],
+        },
+
+        allow_failure: {
+          type: "boolean",
+          nullable: true,
+        },
+      },
+    },
+
+    jobs: {
+      type: "array",
+      nullable: false,
+      items: {
         type: "object",
         nullable: false,
+
         properties: {
-          description: {
+          name: {
             type: "string",
             nullable: false,
-            minLength: 3,
-            maxLength: 255
-          }
-        }
-      },
+          },
 
-      global: {
-        type: "object",
-        nullable: false,
-        properties: {
-
-          // 📦️ Container image to use for all jobs
           image: {
             type: "string",
             nullable: true,
             minLength: 1,
-            maxLength: 4096
+            maxLength: 4096,
           },
 
-          trigger: {
-            type: "object",
+          secrets: {
+            type: "array",
+            items: { type: "string" },
             nullable: true,
-            properties: {
-              branch: {
-                type: ["array", "string"],
-                items: { type: "string" },
-                nullable: true,
-                uniqueItems: true
-              }
-            }
+            uniqueItems: true,
           },
 
           environment: {
@@ -50,7 +117,7 @@ export const NosPipelineSchema: JSONSchemaType<NosJobData> = {
               {
                 type: "object",
                 nullable: true,
-                additionalProperties: { type: "string" } // Additional properties need to be of type string.
+                additionalProperties: { type: "string" }, // Additional properties need to be of type string.
               },
               {
                 type: "object",
@@ -67,164 +134,109 @@ export const NosPipelineSchema: JSONSchemaType<NosJobData> = {
                   value: {
                     type: "string",
                     nullable: false,
-                  }
-                }
-              }
-            ]
+                  },
+                },
+              },
+            ],
           },
 
           allow_failure: {
             type: "boolean",
-            nullable: true
-          }
-        }
-      },
+            nullable: true,
+          },
 
-      jobs: {
-        type: "array",
-        nullable: false,
-        items: {
-          type: "object",
-          nullable: false,
-
-          properties: {
-            name: {
-              type: "string",
-              nullable: false
-            },
-
-            image: {
+          resources: {
+            type: "array",
+            nullable: true,
+            items: {
               type: "string",
               nullable: true,
-              minLength: 1,
-              maxLength: 4096
             },
+          },
 
-            environment: {
+          artifacts: {
+            type: "array",
+            nullable: true,
+            items: {
+              type: "object",
+              nullable: true,
+              properties: {
+                name: {
+                  type: "string",
+                  nullable: false,
+                },
+                path: {
+                  type: "string",
+                  nullable: false,
+                },
+              },
+            },
+          },
+
+          commands: {
+            type: "array",
+            nullable: false,
+            additionalItems: false,
+            items: {
               anyOf: [
                 {
-                  type: "object",
-                  nullable: true,
-                  additionalProperties: { type: "string" } // Additional properties need to be of type string.
+                  type: "string",
+                  nullable: false,
                 },
                 {
                   type: "object",
-                  nullable: true,
+                  nullable: false,
                   properties: {
-                    type: {
+                    cmd: {
                       type: "string",
                       nullable: false,
                     },
-                    endpoint: {
+
+                    working_dir: {
                       type: "string",
-                      nullable: false,
+                      nullable: true,
                     },
-                    value: {
+
+                    allow_failure: {
+                      type: "boolean",
+                      nullable: true,
+                    },
+
+                    resources: {
                       type: "string",
-                      nullable: false,
-                    }
-                  }
-                }
-              ]
-            },
+                      nullable: true,
+                    },
 
-            allow_failure: {
-              type: "boolean",
-              nullable: true
-            },
-
-            resources: {
-              type: "array",
-              nullable: true,
-              items: {
-                type: "string",
-                nullable: true
-              }
-            },
-
-            artifacts: {
-              type: "array",
-              nullable: true,
-              items: {
-                type: "object",
-                nullable: true,
-                properties: {
-                  name: {
-                    type: "string",
-                    nullable: false
-                  },
-                  path: {
-                    type: "string",
-                    nullable: false
-                  }
-                }
-              }
-            },
-
-            commands: {
-              type: "array",
-              nullable: false,
-              additionalItems: false,
-              items: {
-                anyOf: [
-                  {
-                    type: "string",
-                    nullable: false,
-                  },
-                  {
-                    type: "object",
-                    nullable: false,
-                    properties: {
-                      cmd: {
-                        type: "string",
-                        nullable: false
-                      },
-
-                      working_dir: {
-                        type: "string",
-                        nullable: true
-                      },
-
-                      allow_failure: {
-                        type: "boolean",
-                        nullable: true
-                      },
-
-                      resources: {
-                        type: "string",
-                        nullable: true
-                      },
-
-                      artifacts: {
-                        type: "array",
+                    artifacts: {
+                      type: "array",
+                      nullable: true,
+                      items: {
+                        type: "object",
                         nullable: true,
-                        items: {
-                          type: "object",
-                          nullable: true,
-                          properties: {
-                            name: {
-                              type: "string",
-                              nullable: false,
-                            },
+                        properties: {
+                          name: {
+                            type: "string",
+                            nullable: false,
+                          },
 
-                            path: {
-                              type: "string",
-                              nullable: false
-                            }
-                          }
-                        }
+                          path: {
+                            type: "string",
+                            nullable: false,
+                          },
+                        },
                       },
-                    }
-                  }
-                ]
-              }
-            }
-          }
-        }
-      }
+                    },
+                  },
+                },
+              ],
+            },
+          },
+        },
+      },
     },
+  },
 
-    required: ["nosana", "global", "jobs"],
+  required: ["nosana", "global", "jobs"],
 
-    additionalProperties: false
-  }
+  additionalProperties: false,
+};
